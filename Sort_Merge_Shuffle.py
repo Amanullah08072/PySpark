7a2ed772021c1de48a20d2a51782fd22a0bb948b
Amanullah08072 <101502636+Amanullah08072@users.noreply.github.com>
2026-06-22 21:34:06 +0530

    Add Sort Merge Shuffle join implementation
    Implement a Spark job to join fact and dimension tables.

---
+++ Sort_Merge_Shuffle.py
@@ -0,0 +1,18 @@
+# Sort Merge Shuffle join on fact dimension table
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("Practice").getOrCreate()
+
+# dimension_data = [(1,'Tech'),(2,'Fashion'),(3,'living'),(4,'Sports')]
+dimension_data = [(i,f'Tech{(i)}') for i in range(1,1000001)]
+
+dimension_df = spark.createDataFrame(dimension_data,['category_id','category'])
+# dimension_df.show()
+
+fact_data = [(i, (i%1000000) + 1) for i in range(1,10000001)]
+
+fact_df = spark.createDataFrame(fact_data,['transaction_id','category_id'])
+# fact_df.show()
+
+result_df = fact_df.join(dimension_df,on = 'category_id', how='inner')
+result_df.show()
